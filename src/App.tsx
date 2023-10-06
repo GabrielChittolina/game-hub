83fec9c19714c3715855edd4159c4bc5c53d112e
Gabriel Chittolina <chittolina71@gmail.com>
2023-10-06 16:05:26 -0300

    responsive layout with grid from chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={`
+        "nav nav"
+        "aside main"
+      `}
+      templateColumns="210px 1fr"
+      templateRows="80px 1fr"
+      gap={4}
+      h="100vh"
+    >
+      <GridItem bg="tomato" area="nav" />
+      <GridItem bg="gray.800" area="aside" />
+      <GridItem bg="papayawhip" area="main" />
+    </Grid>
+  );
 }
 
 export default App;
