47b145a6035e82356d8a5e8c835123ad28926aa4
Gabriel Chittolina <chittolina71@gmail.com>
2023-10-11 15:06:03 -0300

    fetching games

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
+// temporary fetch
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+// temporary key
+const key = "3a87c61723894f6db7bd9c657f3830e1";
+
+interface Game {
+  id: number;
+  slug: string;
+  name: string;
+  released: string;
+  rating: number;
+  background_image: string;
+  parent_platforms: {
+    platform: {
+      id: number;
+      name: string;
+      slug: string;
+    };
+  }[];
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  // temporary fetch
+  useEffect(() => {
+    axios
+      .get<Game[]>("https://api.rawg.io/api/xgames?key=" + key)
+      .then((res) => {
+        setGames(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Grid
       templateAreas={`
