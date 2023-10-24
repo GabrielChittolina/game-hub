3e7dc641230d499f8d696d4459bddd4dedb33c69
Gabriel Chittolina <chittolina71@gmail.com>
2023-10-24 15:55:19 -0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,8 @@
+import noImagePlaceholder from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string, w: number, h: number) => {
+  if (!url) return noImagePlaceholder;
+
   const crop = `crop/${w}/${h}`;
   const regex = /media\//;
   return url.replace(regex, `media/${crop}/`);
